c70656a2db5c06cf67d0334e26484595b55a0efe
acobb <aecobb53@gmail.com>
2024-04-07 17:08:30 -0600

    making a real lib

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my_base_html_lib',
+    version='1.0.0',
+    author='Andrew Cobb',
+    author_email='aecobb53@gmail.com',
+    description='Creates a default html web page format',
+    keywords='html',
+    url='https://github.com/aecobb53/my_base_html_lib',
+)
