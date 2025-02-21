2cfbc3beda391dadf3c82ec32e0c1da7eb1badc7
acobb <aecobb53@gmail.com>
2025-02-21 15:03:55 -0700

    more tweaks

--- my_base_html_lib/__init__.py
+++ my_base_html_lib/__init__.py
@@ -1,1 +1,11 @@
-from .my_base_document import MyBaseDocument, NavigationContent, SidebarContent, BodyContent, FooterContent
+from .my_base_document import (
+    MyBaseDocument,
+    NavigationContent,
+    SidebarContent,
+    BodyContent,
+    FooterContent,
+    PRIMARY_COLOR,
+    SECONDARY_COLOR,
+    PRIMARY_TEXT_COLOR,
+    SECONDARY_TEXT_COLOR,
+)
